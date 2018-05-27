c6de5a403b32df26dd4563b92fec81cd68d0ddb8
Kathy * <kathy.lambert@lotusdesignsagency.com>
2018-05-27 16:53:26 -0400

    Sucessfully comfirmed the TwitteAPI is responding with the last 25 tweets of specified username.

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -1,8 +1,17 @@
 class TwitterApi
-  def self.our_public_tweets
+  def self.SC_public_tweets
     client.user_timeline('StackCommerce', count: 1, exclude_replies: true, include_rts: false)
   end
-   
+
+  def self.get_last_25_tweets(username, replies=false, rts=false)
+    tweets = client.user_timeline(username, count: 25, exclude_replies: replies, include_rts: rts)
+    
+    tweets.each do |tweet|
+      puts tweet.text
+      puts tweet.created_at
+    end
+  end
+     
   def self.client
     @client  ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
