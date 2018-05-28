3c9282d526d5003fbb2685113170801569c6676d
Kathy * <kathy.lambert@lotusdesignsagency.com>
2018-05-28 00:52:29 -0400

    Corrected routing issue.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'tweet#index'
+  root to: 'home#index'
+  get '/', to: 'home#index'
+  get '/tweets', to: 'tweet#index'
 end
