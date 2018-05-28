dc73528539f97342def3b05e768c6350fdde6bcd
Kathy * <kathy.lambert@lotusdesignsagency.com>
2018-05-28 03:20:43 -0400

    Corrected the tweet controller.

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -6,7 +6,7 @@ class TweetController < ApplicationController
     if !params[:search].blank?
       @tweets = TwitterApi.get_last_25_tweets(params[:search], params[:replies], params[:rts])
     end
-    # puts @tweets.to_s
+    # puts @tweets.first.to_s
     render :index
   end
 end
